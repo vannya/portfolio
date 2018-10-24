c888a65a6edf24d67f635b8dda43d0b967fb482f
Vannya <van.tabbert@gmail.com>
2018-10-24 04:09:43 -0500

    added all the bases of the pages

---
+++ src/pages/About/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styles from "./About.module.css";
+
+class About extends Component {
+  state = {  }
+  render() {
+    return (
+      <section className={styles.about}>About</section>
+    );
+  }
+}
+
+export default About;
---
+++ src/pages/Projects/Projects.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styles from "./Projects.module.css";
+
+class Projects extends Component {
+  state = {  }
+  render() {
+    return (
+      <section className={styles.projects}>Projects</section>
+    );
+  }
+}
+
+export default Projects;
